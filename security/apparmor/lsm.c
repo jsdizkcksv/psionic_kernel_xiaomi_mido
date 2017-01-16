8d4abc2bc99a4d52123e6aaa0e572892298deb0d
John Johansen <john.johansen@canonical.com>
2017-01-16 00:42:50 -0800

    UPSTREAM: apparmor: add ns being viewed as a param to policy_view_capable()
    Prepare for a tighter pairing of user namespaces and apparmor policy
    namespaces, by making the ns to be viewed available and checking
    that the user namespace level is the same as the policy ns level.
    
    This strict pairing will be relaxed once true support of user namespaces
    lands.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -748,7 +748,7 @@ static int param_set_aalockpolicy(const char *val, const struct kernel_param *kp
 
 static int param_get_aalockpolicy(char *buffer, const struct kernel_param *kp)
 {
-	if (!policy_view_capable())
+	if (!policy_view_capable(NULL))
 		return -EPERM;
 	return param_get_bool(buffer, kp);
 }
@@ -762,7 +762,7 @@ static int param_set_aabool(const char *val, const struct kernel_param *kp)
 
 static int param_get_aabool(char *buffer, const struct kernel_param *kp)
 {
-	if (!policy_view_capable())
+	if (!policy_view_capable(NULL))
 		return -EPERM;
 	return param_get_bool(buffer, kp);
 }
@@ -776,14 +776,14 @@ static int param_set_aauint(const char *val, const struct kernel_param *kp)
 
 static int param_get_aauint(char *buffer, const struct kernel_param *kp)
 {
-	if (!policy_view_capable())
+	if (!policy_view_capable(NULL))
 		return -EPERM;
 	return param_get_uint(buffer, kp);
 }
 
 static int param_get_audit(char *buffer, const struct kernel_param *kp)
 {
-	if (!policy_view_capable())
+	if (!policy_view_capable(NULL))
 		return -EPERM;
 
 	if (!apparmor_enabled)
