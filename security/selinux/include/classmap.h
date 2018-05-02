2bc7b21decd8ee6cfd079fd9c5b473518abd7627
Björn Töpel <bjorn.topel@intel.com>
2018-05-02 13:01:22 +0200

    BACKPORT: net: initial AF_XDP skeleton
    Buildable skeleton of AF_XDP without any functionality. Just what it
    takes to register a new address family.
    
    Change-Id: I2116ce900e3ac94ab7329ec9578a3f8eff9d750d
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- security/selinux/include/classmap.h
+++ security/selinux/include/classmap.h
@@ -169,6 +169,8 @@ struct security_class_mapping secclass_map[] = {
 	  { COMMON_CAP2_PERMS, NULL } },
 	{ "bpf",
 	  {"map_create", "map_read", "map_write", "prog_load", "prog_run"} },
+	{ "xdp_socket",
+	  { COMMON_SOCK_PERMS, NULL } },
 	{ "perf_event",
 	  {"open", "cpu", "kernel", "tracepoint", "read", "write"} },
 	{ NULL }
