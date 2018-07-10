ee5ae1ab0294ae379a51c05d12b3567009d8ad41
Al Viro <viro@zeniv.linux.org.uk>
2018-07-10 13:22:28 -0400

    BACKPORT: get rid of cred argument of vfs_open() and do_dentry_open()
    always equal to ->f_cred
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: Ia3fedb0e159a4163078891d6dc62da2324f6e1eb
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/internal.h
+++ fs/internal.h
@@ -113,7 +113,7 @@ extern struct file *do_file_open_root(struct dentry *, struct vfsmount *,
 extern long do_handle_open(int mountdirfd,
 			   struct file_handle __user *ufh, int open_flag);
 extern int open_check_o_direct(struct file *f);
-extern int vfs_open(const struct path *, struct file *, const struct cred *);
+extern int vfs_open(const struct path *, struct file *);
 extern struct file *filp_clone_open(struct file *);
 
 /*
