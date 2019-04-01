966af36796ae899aa196a8dcd72e2d4aa911d3f6
Stanislav Fomichev <sdf@google.com>
2019-04-01 13:57:33 -0700

    BACKPORT: flow_dissector: allow access only to a subset of __sk_buff fields
    Use whitelist instead of a blacklist and allow only a small set of
    fields that might be relevant in the context of flow dissector:
      * data
      * data_end
      * flow_keys
    
    This is required for the eth_get_headlen case where we have only a
    chunk of data to dissect (i.e. trying to read the other skb fields
    doesn't make sense).
    
    Note, that it is a breaking API change! However, we've provided
    flow_keys->n_proto as a substitute for skb->protocol; and there is
    no need to manually handle skb->vlan_present. So even if we
    break somebody, the migration is trivial. Unfortunately, we can't
    support eth_get_headlen use-case without those breaking changes.
    
    Change-Id: Ifb5f50dabbb49f7fe0030562c99ce12503da32fb
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -5361,14 +5361,8 @@ static bool flow_dissector_is_valid_access(int off, int size,
  					   const struct bpf_prog *prog,
  					   struct bpf_insn_access_aux *info)
 {
- 	if (type == BPF_WRITE) {
- 		switch (off) {
- 		case bpf_ctx_range_till(struct __sk_buff, cb[0], cb[4]):
- 			break;
- 		default:
- 			return false;
- 		}
- 	}
+ 	if (type == BPF_WRITE)
+		return false;
 
  	switch (off) {
  	case bpf_ctx_range(struct __sk_buff, data):
@@ -5380,9 +5374,7 @@ static bool flow_dissector_is_valid_access(int off, int size,
  	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
  		info->reg_type = PTR_TO_FLOW_KEYS;
  		break;
- 	case bpf_ctx_range(struct __sk_buff, tc_classid):
- 	case bpf_ctx_range_till(struct __sk_buff, family, local_port):
- 	case bpf_ctx_range(struct __sk_buff, tstamp):
+ 	default:
  		return false;
  	}
 
