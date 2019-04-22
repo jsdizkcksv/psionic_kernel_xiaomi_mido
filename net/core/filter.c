e30aa7560e63a5aec5b57e7a3a6ab1b46e20e714
Stanislav Fomichev <sdf@google.com>
2019-04-22 08:55:44 -0700

    BACKPORT: flow_dissector: switch kernel context to struct bpf_flow_dissector
    struct bpf_flow_dissector has a small subset of sk_buff fields that
    flow dissector BPF program is allowed to access and an optional
    pointer to real skb. Real skb is used only in bpf_skb_load_bytes
    helper to read non-linear data.
    
    The real motivation for this is to be able to call flow dissector
    from eth_get_headlen context where we don't have an skb and need
    to dissect raw bytes.
    
    Change-Id: I5aa984bb223fd2cd05dd0a502f8b4519358124d1
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1494,6 +1494,40 @@ static const struct bpf_func_proto bpf_skb_load_bytes_proto = {
 	.arg4_type	= ARG_CONST_SIZE,
 };
 
+BPF_CALL_4(bpf_flow_dissector_load_bytes,
+	   const struct bpf_flow_dissector *, ctx, u32, offset,
+	   void *, to, u32, len)
+{
+	void *ptr;
+
+	if (unlikely(offset > 0xffff))
+		goto err_clear;
+
+	if (unlikely(!ctx->skb))
+		goto err_clear;
+
+	ptr = skb_header_pointer(ctx->skb, offset, len, to);
+	if (unlikely(!ptr))
+		goto err_clear;
+	if (ptr != to)
+		memcpy(to, ptr, len);
+
+	return 0;
+err_clear:
+	memset(to, 0, len);
+	return -EFAULT;
+}
+
+static const struct bpf_func_proto bpf_flow_dissector_load_bytes_proto = {
+	.func		= bpf_flow_dissector_load_bytes,
+	.gpl_only	= false,
+	.ret_type	= RET_INTEGER,
+	.arg1_type	= ARG_PTR_TO_CTX,
+	.arg2_type	= ARG_ANYTHING,
+	.arg3_type	= ARG_PTR_TO_UNINIT_MEM,
+	.arg4_type	= ARG_CONST_SIZE,
+};
+
 BPF_CALL_5(bpf_skb_load_bytes_relative, const struct sk_buff *, skb,
 	   u32, offset, void *, to, u32, len, u32, start_header)
 {
@@ -4765,7 +4799,7 @@ flow_dissector_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
 {
  	switch (func_id) {
  	case BPF_FUNC_skb_load_bytes:
- 		return &bpf_skb_load_bytes_proto;
+ 		return &bpf_flow_dissector_load_bytes_proto;
  	default:
  		return bpf_base_func_proto(func_id, prog);
  	}
@@ -4840,9 +4874,7 @@ static bool bpf_skb_is_valid_access(int off, int size, enum bpf_access_type type
 		info->reg_type = PTR_TO_SOCK_COMMON_OR_NULL;
 		break;
 	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
-		if (size != sizeof(__u64))
- 			return false;
- 		break;
+		return false;
 	case bpf_ctx_range(struct __sk_buff, tstamp):
 		if (size != sizeof(__u64))
 			return false;
@@ -4870,7 +4902,6 @@ static bool cg_skb_is_valid_access(int off, int size,
 	switch (off) {
 	case bpf_ctx_range(struct __sk_buff, tc_classid):
 	case bpf_ctx_range(struct __sk_buff, data_meta):
-	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
 	case bpf_ctx_range(struct __sk_buff, wire_len):
 		return false;
 	case bpf_ctx_range(struct __sk_buff, data):
@@ -4914,7 +4945,6 @@ static bool lwt_is_valid_access(int off, int size,
 {
 	switch (off) {
 	case bpf_ctx_range(struct __sk_buff, tc_classid):
-	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
 	case bpf_ctx_range_till(struct __sk_buff, family, local_port):
 	case bpf_ctx_range(struct __sk_buff, data_meta):
 	case bpf_ctx_range(struct __sk_buff, wire_len):
@@ -4954,7 +4984,6 @@ static bool sk_filter_is_valid_access(int off, int size,
 	case bpf_ctx_range(struct __sk_buff, tc_classid):
 	case bpf_ctx_range(struct __sk_buff, data):
 	case bpf_ctx_range(struct __sk_buff, data_end):
-	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
 	case bpf_ctx_range_till(struct __sk_buff, family, local_port):
 	case bpf_ctx_range(struct __sk_buff, tstamp):
 	case bpf_ctx_range(struct __sk_buff, wire_len):
@@ -5138,7 +5167,6 @@ static bool tc_cls_act_is_valid_access(int off, int size,
 		case bpf_ctx_range(struct __sk_buff, tc_index):
 		case bpf_ctx_range(struct __sk_buff, priority):
 		case bpf_ctx_range(struct __sk_buff, tc_classid):
-		case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
 		case bpf_ctx_range_till(struct __sk_buff, cb[0], cb[4]):
 		case bpf_ctx_range(struct __sk_buff, tstamp):
 		case bpf_ctx_range(struct __sk_buff, queue_mapping):
@@ -5325,7 +5353,6 @@ static bool sk_skb_is_valid_access(int off, int size,
 				   struct bpf_insn_access_aux *info)
 {
 	switch (off) {
-	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
 	case bpf_ctx_range(struct __sk_buff, tc_classid):
 	case bpf_ctx_range(struct __sk_buff, data_meta):
 	case bpf_ctx_range(struct __sk_buff, wire_len):
@@ -5361,24 +5388,65 @@ static bool flow_dissector_is_valid_access(int off, int size,
  					   const struct bpf_prog *prog,
  					   struct bpf_insn_access_aux *info)
 {
+ 	const int size_default = sizeof(__u32);
+
+	if (off < 0 || off >= sizeof(struct __sk_buff))
+		return false;
+
  	if (type == BPF_WRITE)
 		return false;
 
  	switch (off) {
  	case bpf_ctx_range(struct __sk_buff, data):
+ 	    if (size != size_default)
+			return false;
  		info->reg_type = PTR_TO_PACKET;
- 		break;
+ 		return true;
  	case bpf_ctx_range(struct __sk_buff, data_end):
+ 	    if (size != size_default)
+			return false;
  		info->reg_type = PTR_TO_PACKET_END;
- 		break;
+ 		return true;
  	case bpf_ctx_range_ptr(struct __sk_buff, flow_keys):
+ 	    if (size != sizeof(__u64))
+			return false;
  		info->reg_type = PTR_TO_FLOW_KEYS;
- 		break;
+ 		return true;
  	default:
  		return false;
  	}
+}
 
- 	return bpf_skb_is_valid_access(off, size, type, prog, info);
+static u32 flow_dissector_convert_ctx_access(enum bpf_access_type type,
+					     const struct bpf_insn *si,
+					     struct bpf_insn *insn_buf,
+					     struct bpf_prog *prog,
+					     u32 *target_size)
+
+{
+	struct bpf_insn *insn = insn_buf;
+
+	switch (si->off) {
+	case offsetof(struct __sk_buff, data):
+		*insn++ = BPF_LDX_MEM(BPF_FIELD_SIZEOF(struct bpf_flow_dissector, data),
+				      si->dst_reg, si->src_reg,
+				      offsetof(struct bpf_flow_dissector, data));
+		break;
+
+	case offsetof(struct __sk_buff, data_end):
+		*insn++ = BPF_LDX_MEM(BPF_FIELD_SIZEOF(struct bpf_flow_dissector, data_end),
+				      si->dst_reg, si->src_reg,
+				      offsetof(struct bpf_flow_dissector, data_end));
+		break;
+
+	case offsetof(struct __sk_buff, flow_keys):
+		*insn++ = BPF_LDX_MEM(BPF_FIELD_SIZEOF(struct bpf_flow_dissector, flow_keys),
+				      si->dst_reg, si->src_reg,
+				      offsetof(struct bpf_flow_dissector, flow_keys));
+		break;
+	}
+
+	return insn - insn_buf;
 }
 
 static bool sk_msg_is_valid_access(int off, int size,
@@ -5773,15 +5841,6 @@ static u32 bpf_convert_ctx_access(enum bpf_access_type type,
 				      si->dst_reg, si->src_reg,
 				      offsetof(struct sk_buff, sk));
 		break;
-
-	case offsetof(struct __sk_buff, flow_keys):
- 		off  = si->off;
- 		off -= offsetof(struct __sk_buff, flow_keys);
- 		off += offsetof(struct sk_buff, cb);
- 		off += offsetof(struct qdisc_skb_cb, flow_keys);
- 		*insn++ = BPF_LDX_MEM(BPF_SIZEOF(void *), si->dst_reg,
- 				      si->src_reg, off);
- 		break;
 
 	case offsetof(struct __sk_buff, tstamp):
 		BUILD_BUG_ON(FIELD_SIZEOF(struct sk_buff, tstamp) != 8);
@@ -6697,7 +6756,7 @@ const struct bpf_prog_ops sk_skb_prog_ops = {
 const struct bpf_verifier_ops flow_dissector_verifier_ops = {
  	.get_func_proto		= flow_dissector_func_proto,
  	.is_valid_access	= flow_dissector_is_valid_access,
- 	.convert_ctx_access	= bpf_convert_ctx_access,
+ 	.convert_ctx_access	= flow_dissector_convert_ctx_access,
 };
  
 const struct bpf_prog_ops flow_dissector_prog_ops = {
