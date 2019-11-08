e8b3205c98185ce7a55300b029f0b5f4ec80f047
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-11-08 13:11:27 -0500

    UPSTREAM: ftrace: Add ftrace_find_direct_func()
    As function_graph tracer modifies the return address to insert a trampoline
    to trace the return of a function, it must be aware of a direct caller, as
    when it gets called, the function's return address may not be at on the
    stack where it expects. It may have to see if that return address points to
    the a direct caller and adjust if it is.
    
    Change-Id: I80bd23932c426ec3b2db76538dacd283b972db0a
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -48,6 +48,7 @@ static inline void trace_init(void) { }
 
 struct module;
 struct ftrace_hash;
+struct ftrace_direct_func;
 
 #ifdef CONFIG_FUNCTION_TRACER
 
@@ -281,6 +282,7 @@ static inline void ftrace_kill(void) { }
 int register_ftrace_direct(unsigned long ip, unsigned long addr);
 int unregister_ftrace_direct(unsigned long ip, unsigned long addr);
 int modify_ftrace_direct(unsigned long ip, unsigned long old_addr, unsigned long new_addr);
+struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr);
 #else
 static inline int register_ftrace_direct(unsigned long ip, unsigned long addr)
 {
@@ -294,6 +296,10 @@ static inline int modify_ftrace_direct(unsigned long ip,
 				       unsigned long old_addr, unsigned long new_addr)
 {
 	return -ENODEV;
+}
+static inline struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr)
+{
+	return NULL;
 }
 #endif /* CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS */
 
