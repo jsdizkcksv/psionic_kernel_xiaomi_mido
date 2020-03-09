2c467d882a39dbeaea4ab8d2517dc7fd196a17d9
Lorenz Bauer <lmb@cloudflare.com>
2020-03-09 11:12:38 +0000

    UPSTREAM: bpf: Add sockmap hooks for UDP sockets
    Add basic psock hooks for UDP sockets. This allows adding and
    removing sockets, as well as automatic removal on unhash and close.
    
    Change-Id: Id0e35e63672540015a1789d7b0cee7cc24c597ab
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200309111243.6982-8-lmb@cloudflare.com

--- include/net/udp.h
+++ include/net/udp.h
@@ -362,4 +362,9 @@ void udp_encap_enable(void);
 void udpv6_encap_enable(void);
 #endif
 
+#ifdef CONFIG_BPF_STREAM_PARSER
+struct sk_psock;
+struct proto *udp_bpf_get_proto(struct sock *sk, struct sk_psock *psock);
+#endif /* BPF_STREAM_PARSER */
+
 #endif	/* _UDP_H */
