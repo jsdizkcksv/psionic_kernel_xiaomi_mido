e2a77d9cf5817104773c14d4d2fe29a48c7fd4b3
Christoph Hellwig <hch@lst.de>
2020-05-28 07:12:13 +0200

    BACKPORT: net: add sock_bindtoindex
    Add a helper to directly set the SO_BINDTOIFINDEX sockopt from kernel
    space without going through a fake uaccess.
    
    Change-Id: Idb5a895062810cb89b3b7dca8a774b8b2c6161d5
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -2491,4 +2491,6 @@ extern __u32 sysctl_rmem_default;
 int sockev_register_notify(struct notifier_block *nb);
 int sockev_unregister_notify(struct notifier_block *nb);
 
+int sock_bindtoindex(struct sock *sk, int ifindex);
+
 #endif	/* _SOCK_H */
