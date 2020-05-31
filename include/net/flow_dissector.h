bdc83418fc75395ac434ce030c1b28074692e41e
Jakub Sitnicki <jakub@cloudflare.com>
2020-05-31 10:28:37 +0200

    BACKPORT: flow_dissector: Move out netns_bpf prog callbacks
    Move functions to manage BPF programs attached to netns that are not
    specific to flow dissector to a dedicated module named
    bpf/net_namespace.c.
    
    The set of functions will grow with the addition of bpf_link support for
    netns attached programs. This patch prepares ground by creating a place
    for it.
    
    This is a code move with no functional changes intended.
    
    Change-Id: I42e6a8dfdfca561053e7105f1119ce6f567bff5c
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200531082846.2117903-4-jakub@cloudflare.com

--- include/net/flow_dissector.h
+++ include/net/flow_dissector.h
@@ -7,6 +7,8 @@
 #include <linux/string.h>
 #include <uapi/linux/if_ether.h>
 
+struct bpf_prog;
+struct net;
 struct sk_buff;
 
 /**
@@ -332,5 +334,9 @@ struct bpf_flow_dissector {
 	void			*data;
 	void			*data_end;
 };
+
+#ifdef CONFIG_BPF_SYSCALL
+int flow_dissector_bpf_prog_attach(struct net *net, struct bpf_prog *prog);
+#endif /* CONFIG_BPF_SYSCALL */
 
 #endif
