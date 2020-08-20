31420d738ccd382c389386a79f0f6b34df0667e4
Martin KaFai Lau <kafai@fb.com>
2020-08-20 12:00:27 -0700

    UPSTREAM: tcp: bpf: Add TCP_BPF_RTO_MIN for bpf_setsockopt
    This patch adds bpf_setsockopt(TCP_BPF_RTO_MIN) to allow bpf prog
    to set the min rto of a connection.  It could be used together
    with the earlier patch which has added bpf_setsockopt(TCP_BPF_DELACK_MAX).
    
    A later selftest patch will communicate the max delay ack in a
    bpf tcp header option and then the receiving side can use
    bpf_setsockopt(TCP_BPF_RTO_MIN) to set a shorter rto.
    
    Change-Id: I32e313838af527d12b7e5f16036481bcaefa1e7b
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200820190027.2884170-1-kafai@fb.com

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -405,6 +405,7 @@ void tcp_init_sock(struct sock *sk)
 	INIT_LIST_HEAD(&tp->tsq_node);
 
 	icsk->icsk_rto = TCP_TIMEOUT_INIT;
+	icsk->icsk_rto_min = TCP_RTO_MIN;
 	icsk->icsk_delack_max = TCP_DELACK_MAX;
 	tp->mdev_us = jiffies_to_usecs(TCP_TIMEOUT_INIT);
 	minmax_reset(&tp->rtt_min, tcp_time_stamp, ~0U);
@@ -2444,6 +2445,7 @@ int tcp_disconnect(struct sock *sk, int flags)
 	tp->snd_cwnd = 2;
 	icsk->icsk_probes_out = 0;
 	icsk->icsk_rto = TCP_TIMEOUT_INIT;
+    icsk->icsk_rto_min = TCP_RTO_MIN;
     icsk->icsk_delack_max = TCP_DELACK_MAX;
 	tp->packets_out = 0;
 	tp->snd_ssthresh = TCP_INFINITE_SSTHRESH;
