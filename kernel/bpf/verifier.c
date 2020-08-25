e64e67333b1c464bc08e83d60f1a5e5c334a42e6
Jiri Olsa <jolsa@kernel.org>
2020-08-25 21:21:19 +0200

    UPSTREAM: bpf: Add BTF_SET_START/END macros
    Adding support to define sorted set of BTF ID values.
    
    Following defines sorted set of BTF ID values:
    
      BTF_SET_START(btf_allowlist_d_path)
      BTF_ID(func, vfs_truncate)
      BTF_ID(func, vfs_fallocate)
      BTF_ID(func, dentry_open)
      BTF_ID(func, vfs_getattr)
      BTF_ID(func, filp_close)
      BTF_SET_END(btf_allowlist_d_path)
    
    It defines following 'struct btf_id_set' variable to access
    values and count:
    
      struct btf_id_set btf_allowlist_d_path;
    
    Adding 'allowed' callback to struct bpf_func_proto, to allow
    verifier the check on allowed callers.
    
    Adding btf_id_set_contains function, which will be used by
    allowed callbacks to verify the caller's BTF ID value is
    within allowed set.
    
    Also removing extra '\' in __BTF_ID_LIST macro.
    
    Added BTF_SET_START_GLOBAL macro for global sets.
    
    Change-Id: I3971464103da6a49324fb6ca7fd19aa22c790453
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200825192124.710397-10-jolsa@kernel.org

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -4224,6 +4224,11 @@ static int check_helper_call(struct bpf_verifier_env *env, int func_id, int insn
 		verbose(env, "cannot call GPL-restricted function from non-GPL compatible program\n");
 		return -EINVAL;
 	}
+	
+	if (fn->allowed && !fn->allowed(env->prog)) {
+		verbose(env, "helper call is not allowed in probe\n");
+		return -EINVAL;
+	}
 
 	changes_data = bpf_helper_changes_pkt_data(fn->func);
 
