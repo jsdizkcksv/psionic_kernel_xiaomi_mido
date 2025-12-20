e295913ca3bda6f1bed0e91042097312a722f768
ApartTUSITU <aptusitu@outlook.com>
2025-12-20 22:00:58 +0800

    KSU: Integrate with SUSFS_INLINE_HOOK
    Use this script: https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/blob/mainline/Patches/susfs_inline_hook_patches.sh
    
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Co-authored-by: JackA1ltman <cs2dtzq@163.com>

--- fs/stat.c
+++ fs/stat.c
@@ -18,6 +18,9 @@
 #include <linux/susfs_def.h>
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
@@ -105,12 +108,30 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
+#ifdef CONFIG_KSU_SUSFS
+extern bool ksu_su_compat_enabled __read_mostly;
+extern bool __ksu_is_allow_uid_for_current(uid_t uid);
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
+#ifdef CONFIG_KSU_SUSFS
+	if (likely(susfs_is_current_proc_umounted()) ||
+	    !ksu_su_compat_enabled) {
+		goto orig_flow;
+	}
+
+	if (unlikely(__ksu_is_allow_uid_for_current(current_uid().val))) {
+		ksu_handle_stat(&dfd, &filename, &flags);
+	}
+
+orig_flow:
+#endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
