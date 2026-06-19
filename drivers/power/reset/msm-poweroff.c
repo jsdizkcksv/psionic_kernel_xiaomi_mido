21912c9c80baf1df98954d6ed23d9c94cbdb9654
jsdizkcksv <3531236823@qq.com>
2026-06-19 16:39:39 +0800

    [debug]: qcom: add bpf zygote loading failed check
    Change-Id: I2b2170e407dc45fb3f98b3dfbe8a01e789085e5c

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -325,7 +325,7 @@ static void msm_restart_prepare(const char *cmd)
 	}
 
 	if (cmd != NULL) {
-		if (!strncmp(cmd, "bootloader", 10) || !strncmp(cmd, "recovery", 8)) {
+		if (!strncmp(cmd, "bootloader", 10) || !strncmp(cmd, "bpfloader-failed", 16) || !strncmp(cmd, "zygote-failed", 13) || !strncmp(cmd, "recovery", 8)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_RECOVERY);
 			__raw_writel(0x77665502, restart_reason);
