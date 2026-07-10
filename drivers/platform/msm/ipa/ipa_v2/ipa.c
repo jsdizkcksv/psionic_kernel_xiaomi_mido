18c18671702c1155e1348d7bda0117426380e13c
jsdizkcksv <3531236823@qq.com>
2026-07-10 10:43:02 +0800

    Revert "msm: ipa: Don't let ipa_v3 prevent this to suspend"
    This reverts commit d4adbd780763a9e970a3e50c99ef72ac382aa9fb.

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -5075,7 +5075,7 @@ int ipa2_ap_suspend(struct device *dev)
 
 	IPADBG("Enter...\n");
 
-#ifndef CONFIG_IPA_WAKELOCK
+#if 0
 	/* In case there is a tx/rx handler in polling mode fail to suspend */
 	for (i = 0; i < ipa_ctx->ipa_num_pipes; i++) {
 		if (ipa_ctx->ep[i].sys &&
@@ -5086,7 +5086,7 @@ int ipa2_ap_suspend(struct device *dev)
 		}
 	}
 #endif
-
+	
 	/* release SPS IPA resource without waiting for inactivity timer */
 	atomic_set(&ipa_ctx->sps_pm.eot_activity, 0);
 	ipa_sps_release_resource(NULL);
