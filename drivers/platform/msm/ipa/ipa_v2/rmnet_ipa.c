ef935e09d3a3ba4a99dec726a4a0cd8a69084172
jsdizkcksv <3531236823@qq.com>
2026-07-10 10:44:22 +0800

    Revert "msm: ipa_v2: Fix callback function type for ndo_start_xmit"
    This reverts commit 80a822d825acba9f1af0eaf0b014b90b3cea82c8.

--- drivers/platform/msm/ipa/ipa_v2/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/rmnet_ipa.c
@@ -1073,9 +1073,9 @@ static int ipa_wwan_change_mtu(struct net_device *dev, int new_mtu)
  * later
  * -EFAULT: Error while transmitting the skb
  */
-static netdev_tx_t ipa_wwan_xmit(struct sk_buff *skb, struct net_device *dev)
+static int ipa_wwan_xmit(struct sk_buff *skb, struct net_device *dev)
 {
-	netdev_tx_t ret = NETDEV_TX_OK;
+	int ret = 0;
 	bool qmap_check;
 	struct wwan_private *wwan_ptr = netdev_priv(dev);
 	struct ipa_tx_meta meta;
